7679363842dfcca0a9784f93ba840ccb73ef4394
pygamer0 <maanasyt@gmail.com>
2021-08-16 12:21:10 +0530

    basic splitting

--- src/stparser.py
+++ src/stparser.py
@@ -3,15 +3,15 @@ The parser module for 51AC8
 """
 import re
 
-def tokenise(string):
+def split(string):
     """
-    Removes comments and tokenises code.
+    Removes comments and splits code.
 
     string: str
-     The code to tokenise
+     The code to split
 
     returns: list(str)
-     tokenised code
+     splitted code
     """
     string = re.sub(r'#.*', '', string)
     string = (
@@ -22,4 +22,27 @@ def tokenise(string):
             .replace('{', ' { ')
             .replace('}', ' } ')
             )
+    for char in "+=÷×":
+        string = re.sub(r'(K|\\){0}' + '\\' + char, ' ' + char + ' ', string)
     string = re.sub(r'(K.)',  r' \1 ', string)
+    
+    splitted = []
+    temp = ''
+
+    i = 0
+    while i < len(string):
+        char = string[i]
+        i += 1
+
+        if char == ' ' or char == '\n':
+            if temp != '':
+                splitted.append(temp)
+            temp = ''
+            continue
+
+        temp += char
+
+    return splitted
+
+if __name__ == "__main__":
+    print(split('2 1 1+-'))
