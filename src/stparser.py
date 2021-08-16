3212e6109b7da47adece6901ef518ba77aca3aec
pygamer0 <maanasyt@gmail.com>
2021-08-16 18:47:27 +0530

    tokenisation

--- src/stparser.py
+++ src/stparser.py
@@ -1,7 +1,27 @@
 """
 The parser module for 51AC8
 """
+from enum import Enum, auto
 import re
+
+COMMANDS = '+-×÷'
+
+class TType(Enum):
+    NUMBER = auto()
+    COMMAND = auto()
+    LITERAL = auto()
+
+class Token:
+    def __init__(self, value, _t):
+        self.value = value
+        self.type = _t
+        self.things = {}
+
+    def update(self, things):
+        self.things = things
+
+    def __repr__(self):
+        return f"Token({repr(self.value)}, {self.type}, {self.things})"
 
 def split(string):
     """
@@ -22,7 +42,7 @@ def split(string):
             .replace('{', ' { ')
             .replace('}', ' } ')
             )
-    for char in "+=÷×":
+    for char in COMMANDS:
         string = re.sub(r'[^K|\\]' + '\\' + char, ' ' + char + ' ', string)
     string = re.sub(r'(K.)',  r' \1 ', string)
     
@@ -46,5 +66,30 @@ def split(string):
 
     return splitted
 
+def tokenise(splitted):
+    """
+    converts the list of strings
+     to tokens
+
+    splitted: list(str)
+    
+    returns: list(Token)
+    """
+    tokens = []
+
+    i = -1
+    for v in splitted:
+        try: token = Token(int(v), TType.NUMBER)
+        except:
+            try: token = Token(float(v), TType.NUMBER)
+            except:
+                token = Token(v, TType.COMMAND)
+        if splitted[i] == '\\':
+            token = Token(v, TType.LITERAL)
+        tokens.append(token)
+        i += 1
+
+    return tokens
+
 if __name__ == "__main__":
-    print(split('2 1 1+-'))
+    print(tokenise(split('2 3.45 0.01 3e100 1 1+-')))
