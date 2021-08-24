9ebbc3759fba62aa9a198a79ea9c7a6514ac3a14
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2021-08-24 14:26:52 +0530

    dyadic vectorisation and changes

--- src/stutils.py
+++ src/stutils.py
@@ -5,6 +5,7 @@ This module is standalone and does not depend on anything except the
 ones found in the python standard library.
 """
 from collections import deque
+from itertools import zip_longest
 
 def depth(l):
     """
@@ -13,7 +14,7 @@ def depth(l):
     [] -> 1
     [[4, []], 3] -> 3
     
-    l: list(any)
+    l: list(any) | any
     """
     if not isinstance(l, list):
         return 0
@@ -30,7 +31,7 @@ def reduce_first(f, l):
     
     f: dyadic function
     
-    l: list(any)
+    l: list(any) | any
     """
     if not isinstance(l, list):
         return l
@@ -39,6 +40,32 @@ def reduce_first(f, l):
         res = f(res, x)
     return res
 
+def vectorise2(f, x, y):
+    """
+    Jelly's generic vectorisation function.
+    Ported from Jelly to Python
+    
+    Credit: caird coinheringaahing's jelly 
+     answer https://codegolf.stackexchange.com/a/220832/103854
+    
+    f: dyadic function
+    
+    x: list(any) | any
+    
+    y: list(any) | any
+    """
+    dx = depth(x)
+    dy = depth(y)
+    if dx == dy:
+        if dx != 0:
+            return [vectorise2(f, a, b) for a,b in zip_longest(x, y, fillvalue=0)]
+        else:
+            return f(x, y)
+    else:
+        if dx < dy:
+            return [vectorise2(f, x, b) for b in y]
+        else:
+            return [vectorise2(f, a, y) for a in x]
 
 def reduce(f, l):
     """
@@ -46,7 +73,7 @@ def reduce(f, l):
     
     f: dyadic function
     
-    l: list(any)
+    l: list(any) | any
     """
     if not isinstance(l, list):
         return l
@@ -78,9 +105,12 @@ def reshape(shape, l):
     shape: list(int)
      the shape we want to reshape to
     
-    l: list
+    l: list(any) | any
     """
-    if not isinstance(l, deque): l = deque(l)
+    if not (isinstance(l, list) or isinstance(l, deque)):
+        return l
+    if not isinstance(l, deque):
+        l = deque(l)
     if len(shape) == 1:
          return [_next(l) for x in range(shape[0])]
     else:
